8d6b967b210ba493236f19a7c3c80ff8a574c598
hosein <h.farahani@outlook.com>
2016-07-29 22:19:53 +0430

    Output customizer style options

--- inc/customizer.php
+++ inc/customizer.php
@@ -24,3 +24,40 @@ function mention_wp_customize_preview_js() {
 	wp_enqueue_script( 'mention_wp_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'mention_wp_customize_preview_js' );
+
+
+function header_output() {
+  ?>
+  <!--Customizer CSS--> 
+  <style type="text/css">
+       <?php generate_css('h1.site-title', 'color', 'header_textcolor', '#'); ?> 
+  </style> 
+  <!--/Customizer CSS-->
+  <?php
+}
+
+
+/**
+ * This will generate a line of CSS for use in header output. If the setting
+ * ($mod_name) has no defined value, the CSS will not be output.
+ */
+function generate_css( $selector, $style, $mod_name, $prefix='', $postfix='', $echo=true ) {
+  $return = '';
+  $mod = get_theme_mod($mod_name);
+  if ( ! empty( $mod ) ) {
+     $return = sprintf('%s { %s:%s; }',
+        $selector,
+        $style,
+        $prefix.$mod.$postfix
+     );
+     if ( $echo ) {
+        echo $return;
+     }
+  }
+  return $return;
+}
+
+
+
+// Output custom CSS to live site
+add_action( 'wp_head' , 'header_output' );
