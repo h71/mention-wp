c9f12d4702729b0630cd976148f364bf0b6e2ed1
hosein <h.farahani@outlook.com>
2016-07-29 22:08:59 +0430

    Initial header and footer, title markup changed in header

---
+++ footer.php
@@ -0,0 +1,28 @@
+        <footer>
+          <div class="firstfooter">
+           <div class="footerMenu">
+             <?php //navigation here ?>
+           </div>
+          </div>
+          <div class="lastfooter">
+            <div class="lastfooterSubcontainer">
+              <div class="connect">
+                <span><?php //Connect with us: custom text in customizer here ?></span>
+                <!-- Here put link to your social profiles (href="http://example.com/") -->
+                <a class="lastfooterSocial" target="_blank" href="#"><i class="icon fa fa-twitter"></i></a>
+                <a class="lastfooterSocial" target="_blank" href="#"><i class="icon fa fa-facebook"></i></a>
+                <a class="lastfooterSocial" target="_blank" href="#"><i class="icon fa fa-google-plus"></i></a>
+              </div>
+              <div class="creators">
+                <?php //srip tags allowed tags here for copy right text; ?>
+                <span class="themeCreator">Powered by <a href="https://ghost.org/">Ghost</a> , crafted by <a href="http://vanila.io/" title="Mobile App development">Vanila</a></span>
+
+              </div>
+            </div>
+          </div>
+        </footer>
+    </div>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>" />
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- favicon -->
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,700' rel='stylesheet' type='text/css'>
+    <!-- pingback -->
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div class="blogWrapper">
+    <div class="progressbarWrapper">
+      <div class="progressbar"></div>
+    </div>
+        <header class="topbar">
+          <div class="topbarCont">
+            <?php if ( get_header_image() ) : ?>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
+            <?php endif; ?>
+          </div>
+          <div class="topbarCont">
+            <div class="topbarInfo">
+              <!-- Add your own tagline here -->
+              <span><?php bloginfo( 'description' ); ?></span>
+              <!-- Also, change title, link and text of the button.-->
+              <a title="Get free quote" href="#"><?php ### ?></a>
+            </div>
+          </div>
+        </header>
